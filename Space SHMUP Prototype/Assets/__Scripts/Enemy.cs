8586b8435d335cc658714d38407ddfb9afca9359
Dalton McPhaden <dalton.mcphaden@hotmail.com>
2019-02-26 09:07:56 -0500

    Final Commit
    Transferring from other repository.

---
+++ Space SHMUP Prototype/Assets/__Scripts/Enemy.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float Speed = 0.1f;
+    public float fireRate = 0.3f;
+    public float health = 10;
+    public int score = 100;
+
+    protected BoundsCheck bndCheck;
+
+    public Vector3 pos {
+        get {
+            return(this.transform.position);
+        }
+        set {
+            this.transform.position = value;
+        }
+    }
+
+    void Awake() {
+        bndCheck = GetComponent<BoundsCheck>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+
+        if (bndCheck != null && bndCheck.offDown) {
+            Destroy(gameObject);
+        }
+    }
+
+    public virtual void Move() {
+        Vector3 tempPos = pos;
+        tempPos.y -= Speed + Time.deltaTime;
+        pos = tempPos;
+    }
+}
---
+++ Space SHMUP Prototype/Assets/__Scripts/Enemy_1.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_1 : Enemy
+{
+    private float x0, y0; 
+
+   // Start is called before the first frame update
+    void Start()
+    {
+        x0 = pos.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+       public override void Move()
+    {
+        Vector3 tempPos = pos;
+        if(x0 > 0)
+        tempPos.x -= Speed + Time.deltaTime;
+        if (x0 < 0)
+        tempPos.x += Speed + Time.deltaTime;
+
+        pos = tempPos;
+
+        base.Move();
+
+        if (bndCheck != null && bndCheck.offDown || bndCheck.offLeft || bndCheck.offRight)
+        {
+            if (pos.y < bndCheck.camHeight - bndCheck.radius)
+            {
+                Destroy(gameObject);
+            }
+        }
+
+    }
+}
