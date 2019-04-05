c25f80e6eb6c1b4e71e52696fe2d1b4de112bc7a
LMassecar <lmassec@uwo.ca>
2019-04-05 16:52:34 -0400

    Colour Switching
    Change colour functionality added.

---
+++ Space SHMUP Prototype/Assets/__Scripts/Colour.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Colour : MonoBehaviour
+{
+    public Material shipMat;
+    
+    void Start()
+    {
+        shipMat.color = Color.white;
+    }
+
+    public void MakeGreen()
+    {
+        shipMat.color = Color.green;
+    }
+    public void MakeBlue()
+    {
+        shipMat.color = Color.blue;
+    }
+    public void MakeRed()
+    {
+        shipMat.color = Color.red;
+    }
+}
